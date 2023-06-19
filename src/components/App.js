ae30ff87f588fd226090fa55cbfe5251b2b29d09
jasbhatia <94101161+jasbhatia@users.noreply.github.com>
2023-06-19 18:17:11 +0530

    submitting code-Mon Jun 19 2023 18:17:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Form from "./Form";
 
 const App = () => {
+  let [name, setName] = useState("Enter Name");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  function submitForm(e) {
+    e.preventDefault();
+    let obj = {
+      name,
+      email,
+      password,
+    };
+    console.log(obj);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <form onSubmit={submitForm}>
+        <Form
+          type={"text"}
+          placeholder={"Name"}
+          updateName={setName}
+          label={"Name"}
+        />
 
-export default App
+        <br></br>
+        <Form
+          type={"email"}
+          placeholder={"e@mail.com"}
+          updateName={setEmail}
+          label={"Email"}
+        />
+        <br></br>
+        <Form
+          type={"password"}
+          placeholder={""}
+          updateName={setPassword}
+          label={"Password"}
+        />
+        <br></br>
+        {/* <Form /> */}
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* <h1>Hello world</h1> */}
+    </div>
+  );
+};
+
+export default App;
